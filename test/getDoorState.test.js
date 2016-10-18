b791d445c141b2f30f3e64374b70f632af6f4fa4
floodpeak <hongfengzhou@yahoo.com>
2016-10-18 22:35:49 +0800

    ut, for #2, failed

--- test/getDoorState.test.js
+++ test/getDoorState.test.js
@@ -5,4 +5,7 @@ describe('100 doors', ()=>{
   it('should be open for #1', ()=>{
     expect(getDoorState(1)).to.be.true
   })
+  it('should be closed for #2', ()=>{
+    expect(getDoorState(2)).to.be.false
+  })
 })
